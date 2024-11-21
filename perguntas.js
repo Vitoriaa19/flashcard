2231d65e7016c12f1e1c8c5998e0c4b9395d585a
Vivi <00001083537544sp@al.educacao.sp.gov.br>
2024-11-21 09:02:15 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quando começou a Segunda Guerre Mundial?',
+    '1939'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasília'
+)
+
+criaCartao(
+    'Entreterimento',
+    'Qual é o reality show mais famoso do mundo?',
+    'Love Isalnd'
+)
+
+criaCartao(
+    'Arte',
+    'Quem pintou o quadro noite estrelada',
+    'Van Gogh'
+)
